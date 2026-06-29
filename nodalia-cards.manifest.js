a6399b6301eeb2775f130d373151e54249e37bea
s3rp1 <s3rp1@icloud.com>
2026-06-29 14:14:05 +0200

    Release v1.3.3

--- nodalia-cards.manifest.js
+++ nodalia-cards.manifest.js
@@ -1,16 +1,16 @@
 export default {
-  "pkgVersion": "1.3.3-alpha.4",
-  "contentSha256_12": "46abf01d8d24",
+  "pkgVersion": "1.3.3",
+  "contentSha256_12": "c7049454dd14",
   "file": "nodalia-cards.bundle.js",
   "loaderFile": "nodalia-cards.js",
-  "hacsFile": "nodalia-cards-1.3.3-alpha.4.js",
-  "splitCoreFile": "nodalia-cards-core-1.3.3-alpha.4.js",
+  "hacsFile": "nodalia-cards-1.3.3.js",
+  "splitCoreFile": "nodalia-cards-core-1.3.3.js",
   "splitCoreSha256_12": "ddd82f25aded",
-  "splitSuiteFile": "nodalia-cards-suite-1.3.3-alpha.4.js",
-  "splitSuiteSha256_12": "73226fe4b232"
+  "splitSuiteFile": "nodalia-cards-suite-1.3.3.js",
+  "splitSuiteSha256_12": "3201424b96ca"
 };
-export const pkgVersion = "1.3.3-alpha.4";
-export const contentSha256_12 = "46abf01d8d24";
+export const pkgVersion = "1.3.3";
+export const contentSha256_12 = "c7049454dd14";
 export const file = "nodalia-cards.bundle.js";
-export const splitCoreFile = "nodalia-cards-core-1.3.3-alpha.4.js";
-export const splitSuiteFile = "nodalia-cards-suite-1.3.3-alpha.4.js";
+export const splitCoreFile = "nodalia-cards-core-1.3.3.js";
+export const splitSuiteFile = "nodalia-cards-suite-1.3.3.js";
