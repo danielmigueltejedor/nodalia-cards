59fb2cb55e3ea614f1316fbec72dd0c995ec1f90
s3rp1 <s3rp1@icloud.com>
2026-08-11 03:23:19 +0200

    ci: consolidate action updates and release artifacts

--- nodalia-cards-2.1.2.js
+++
@@ -1,2 +0,0 @@
-import "./nodalia-cards-2.1.3-alpha.3.js";
-if(typeof window!=="undefined"){window.__NODALIA_LOADER__={"mode":"compat","pkgVersion":"2.1.3-alpha.3","contentSha256_12":"89b8f1a34f68","file":"nodalia-cards-2.1.2.js","targetFile":"nodalia-cards-2.1.3-alpha.3.js","fallbackFile":"nodalia-cards.js"};}
